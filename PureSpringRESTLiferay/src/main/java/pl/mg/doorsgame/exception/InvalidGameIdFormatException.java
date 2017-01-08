2e013275087ea33380020f8b7897cf25cf4cb469
mgzik <mgzik@ospartner.pl>
2017-01-08 17:58:15 +0100

    exception

---
+++ PureSpringRESTLiferay/src/main/java/pl/mg/doorsgame/exception/InvalidGameIdFormatException.java
@@ -0,0 +1,12 @@
+package pl.mg.doorsgame.exception;
+
+/**
+ * 
+ * @author Maciej Gzik
+ *
+ */
+public class InvalidGameIdFormatException extends Exception {
+
+    private static final long serialVersionUID = -158667346261010489L;
+
+}
